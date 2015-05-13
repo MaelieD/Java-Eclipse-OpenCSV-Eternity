1553e05dc4c5c9f72932afb3711495e45742e4ef
Erreur303 <cordelia.bartmann@gmail.com>
2015-05-14 00:56:40 +0300

    Tentative d'ajout de deux panels pour mettre puzzle à gauche et les commandes à droite dans la fenêtre.

--- src/main/java/com/icauda/piece/project/Frame.java
+++ src/main/java/com/icauda/piece/project/Frame.java
@@ -1,19 +1,36 @@
 package com.icauda.piece.project;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import java.util.List;
+import java.util.ArrayList;
+
 
 import com.icauda.piece.project.domain.SimplePiece;
 
 import java.awt.*;
-import java.util.List;
 
 public class Frame extends JFrame {
 	
 	public int p = 0;
+	public String str1 = null, str2 = null;
+	private ArrayList<String> resourcesPath = new ArrayList<String>();  
 	private JFrame fenetre;
-	JPanel panel = new JPanel();
+	private JTable tableau;
+	private JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	private JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	private JLabel label = new JLabel();
 	
-	private final static String RESOURCES_PATH = "src/main/resources/image";
+	// champs de texte pour entre les coord des pieces a tourner / echanger
+	private JTextField textFieldCoordX;
+	private JTextField textFieldCoordY;
+	private JTextField textFieldCoordXExI;
+	private JTextField textFieldCoordYExI;
+	private JTextField textFieldCoordXExF;
+	private JTextField textFieldCoordYExF;
+	
+	private final static String RESOURCES_PATH = "src/main/resources/image/";
 	
 	public Frame(List<SimplePiece> pieces){
 		super();
@@ -25,45 +42,167 @@ public class Frame extends JFrame {
 
 		// INITALISE FENETRE
 		fenetre = new JFrame("Eternity");
-		fenetre.setSize(680,730); 
+		//fenetre.setSize(680,730);
+		fenetre.setSize(1000,1000); 
 		fenetre.setLocationRelativeTo(null); // On centre la fenêtre sur l'écran
 		fenetre.setResizable(true); // On permet le redimensionnement
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // On dit à l'application de se fermer lors du clic sur la croix
 		fenetre.setVisible(true);
 		
-		panel.setLayout(new FlowLayout());
+		panel1.setLayout(new FlowLayout());
 		JLabel label = new JLabel("Bienvenue dans ma modeste application");
-		panel.add(label);
+		panel1.add(label);
 		
 		// CREATION TABLEAU
 		TableauPiece tab = TableauPiece.getInstance(pieces);
-		
-		for(int i = 0 ; i < 3 ; i++) {
-			for(int j = 0 ; j < 3 ; j++) {
+		for(int i = 0 ; i < tab.tabPiece.length ; i++) {
+			for(int j = 0 ; j < tab.tabPiece[0].length ; j++) {
 				//label = new JLabel("tab["+i+"]["+j+"] = "+ tab.tabPiece[i][j]+" -> "+pieces.get(p));
+				str1 = tab.tabPiece[i][j] + ".png";
+				str2 = RESOURCES_PATH + str1;
 				
-				p++;
+				resourcesPath.add(str2);
+				//System.out.println(resourcesPath.get(p)); // ca fonctionne !
+				
+				//p++;
 			}
 		}
 		
-		/*Object[][] data = {
+		for(String name : resourcesPath){
+			System.out.println(name);
+		}
+		
+
+		final ImageIcon icon1 = new ImageIcon(resourcesPath.get(0));
+		final ImageIcon icon2 = new ImageIcon(resourcesPath.get(1));
+		final ImageIcon icon3 = new ImageIcon(resourcesPath.get(2));
+		final ImageIcon icon4 = new ImageIcon(resourcesPath.get(3));
+		final ImageIcon icon5 = new ImageIcon(resourcesPath.get(4));
+		final ImageIcon icon6 = new ImageIcon(resourcesPath.get(5));
+		final ImageIcon icon7 = new ImageIcon(resourcesPath.get(6));
+		final ImageIcon icon8 = new ImageIcon(resourcesPath.get(7));
+		final ImageIcon icon9 = new ImageIcon(resourcesPath.get(8));
+	
+		Object[][] data = {
 				{icon1, icon2, icon3},
 				{icon4, icon5, icon6},
 				{icon7, icon8, icon9}
+		};
+		
+		String title[] = {"0","1","2"};
+		panel1.add(tableau = new JTable(data, title));
+		
+		DefaultTableModel model;
+		model = new DefaultTableModel(data, title);
+		tableau = new JTable(model){
+			public Class getColumnClass(int column){
+				return getValueAt(0, column).getClass();				
+			}
+		};
+		
+		tableau.setRowHeight(220);
+		tableau.setRowMargin(1);
+		tableau.getColumnModel().getColumn(0).setResizable(false);
+		tableau.getColumnModel().getColumn(1).setResizable(false);
+		tableau.getColumnModel().getColumn(2).setResizable(false);
+		//tableau.getColumnModel().getColumn(3).setResizable(false);
 
-		};*/
+		fenetre.getContentPane().add(tableau.getTableHeader(), BorderLayout.NORTH);
+		fenetre.getContentPane().add(tableau, BorderLayout.CENTER);
+		
+		pack();
+		
+		fenetre.setVisible(true);
+		
+		//initComposant();
+		
+		fenetre.setContentPane(buildContentPane());
 	}
 	
-
+	/*private void initComposant(){
+	    //On définit la police d'écriture à utiliser
+	    Font police = new Font("Arial", Font.BOLD, 20);
+	    label = new JLabel("0");
+	    label.setFont(police);
+	    //On aligne les informations à droite dans le JLabel
+	    label.setHorizontalAlignment(JLabel.RIGHT);
+	    label.setPreferredSize(new Dimension(220, 20));
+	    JPanel operateur = new JPanel();      
+	    operateur.setPreferredSize(new Dimension(55, 225));
+	    JPanel chiffre = new JPanel();
+	    chiffre.setPreferredSize(new Dimension(165, 225));
+	    JPanel panEcran = new JPanel();
+	    panEcran.setPreferredSize(new Dimension(220, 30));
+	}*/
 	
-	/*private ImageIcon icon1 = new ImageIcon("image/piece1.png");
-	private ImageIcon icon2 = new ImageIcon("image/piece2.png");
-	private ImageIcon icon3 = new ImageIcon("image/piece3.png");
-	private ImageIcon icon4 = new ImageIcon("image/piece4.png");
-	private ImageIcon icon5 = new ImageIcon("image/piece5.png");
-	private ImageIcon icon6 = new ImageIcon("image/piece6.png");
-	private ImageIcon icon7 = new ImageIcon("image/piece7.png");
-	private ImageIcon icon8 = new ImageIcon("image/piece8.png");
-	private ImageIcon icon9 = new ImageIcon("image/piece9.png");*/
+	private JPanel buildContentPane() {
+		panel2.setLayout(new FlowLayout());
+		
+		textFieldCoordX = new JTextField();
+		textFieldCoordX.setColumns(1);
+		
+		textFieldCoordY = new JTextField();
+		textFieldCoordY.setColumns(1);
+		
+		panel2.add(textFieldCoordX);
+		panel2.add(textFieldCoordY);
+		
+		/*label = new JLabel("Coordonnées de la piece (X, Y)");
+		
+		panel.add(label);
+
+		JButton boutonTourneDroite = new JButton("Tourne Droite");
+		boutonTourneDroite.addActionListener(new BoutonTourneDroite());
+		
+		JButton boutonTourneGauche = new JButton("Tourne Gauche");
+		boutonTourneGauche.addActionListener(new BoutonTourneGauche());
+		
+		panel.add(boutonTourneGauche);
+		panel.add(boutonTourneDroite);
+		
+		labelSpace1 = new JLabel("Echange de deux pieces : 1ere");
+		panel.add(labelSpace1);
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+		textFieldCoordXExI = new JTextField();
+		textFieldCoordXExI.setColumns(1);
+		
+		textFieldCoordYExI = new JTextField();
+		textFieldCoordYExI.setColumns(1);
+		
+		panel.add(textFieldCoordXExI);
+		panel.add(textFieldCoordYExI);
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+		labelSpace2 = new JLabel("   2eme");
+		panel.add(labelSpace2);
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+		textFieldCoordXExF = new JTextField();
+		textFieldCoordXExF.setColumns(1);
+		
+		textFieldCoordYExF = new JTextField();
+		textFieldCoordYExF.setColumns(1);
+		
+		panel.add(textFieldCoordXExF);
+		panel.add(textFieldCoordYExF);
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+		
+		JButton boutonEchange = new JButton("Echange !");
+		boutonEchange.addActionListener(new BoutonEchange());
+		
+		panel.add(boutonEchange);
+		
+		JButton boutonVerification = new JButton("Verification du Puzzle");
+		boutonVerification.addActionListener(new BoutonVerification());
+		
+		panel.add(boutonVerification);
+		
+		labelPhraseFin = new JLabel("Vous n'avez pas gagné.");
+		panel.add(labelPhraseFin);
+		*/
+		return panel2;
+	}
 
 }
--- src/main/java/com/icauda/piece/project/TableauPiece.java
+++ src/main/java/com/icauda/piece/project/TableauPiece.java
@@ -7,15 +7,15 @@ import com.icauda.piece.project.domain.SimplePiece;
 public class TableauPiece {
 
 	private static TableauPiece instance;
-	public static SimplePiece[][] tabPiece = new SimplePiece[3][3];
+	public static String[][] tabPiece = new String[3][3];
 	//private static JLabel[][] tabLabel = new JLabel[3][3];
 	private int p = 0;
 	
 	private TableauPiece(List<SimplePiece> pieces) {
 		for(int i = 0 ; i < 3 ; i++) {
 			for(int j = 0 ; j < 3 ; j++) {
-				tabPiece[i][j] = pieces.get(p);
-				System.out.println("tab["+i+"]["+j+"] = "+ tabPiece[i][j]+" -> "+pieces.get(p));
+				tabPiece[i][j] = pieces.get(p).toString();
+				System.out.println("tab["+i+"]["+j+"] = "+ tabPiece[i][j]+" -> "+pieces.get(p)); // test
 				p++;
 			}
 		}
@@ -26,10 +26,5 @@ public class TableauPiece {
 			instance = new TableauPiece(pieces);
 		return instance;
 	}
-	
-		/*for (SimplePiece piece : pieces) {
-			for (SimplePiece)
-			System.out.println(piece);
-		}*/
 	
 }
